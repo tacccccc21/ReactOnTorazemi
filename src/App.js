b75c987731ce457ef44dd8bfb558110695ce799f
tac <tacccccc21@gmail.com>
2020-10-03 00:24:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React on torazemi
+          Learn React aaaon torazemi
         </a>
       </header>
     </div>
