681df1ea7e413d848f6e36a4d9806d37e4e19b79
tac <tacccccc21@gmail.com>
2020-10-06 17:07:14 +0900

    Update blog.jsx

--- src/blog.jsx
+++ src/blog.jsx
@@ -1,1 +1,14 @@
 import React from 'react'
+
+class blog extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return{
+      <div>
+        <Article />
+      </div>
+    }
+  }
+}
